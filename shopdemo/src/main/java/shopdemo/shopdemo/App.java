f3d8249252b7dded785a64ec10ab5021b98bf309
lakshmineha <srilaxmi550@gmail.com>
2023-12-16 15:17:54 +0530

    comm

---
+++ shopdemo/src/main/java/shopdemo/shopdemo/App.java
@@ -0,0 +1,13 @@
+package shopdemo.shopdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
